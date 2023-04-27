2c72593fac6d02cdd26c577982346c9d9eb02834
Joseph Hill <joseph.hill1@outlook.com>
2023-04-27 15:18:22 -0400

    Created Services and Web middleware extensions.

--- BlogLab.Models/Exception/ApiException.cs
+++ BlogLab.Models/Exception/ApiException.cs
@@ -1,16 +1,16 @@
-namespace BlogLab.Models.Exception 
+using Newtonsoft.Json;
+
+namespace BlogLab.Models.Exception
 {
     public class ApiException
     {
-        public ApiException(int statusCode, string message, string details)
-        {
-            StatusCode = statusCode;
-            Message = message;
-            Details = details;
-        }
-
         public int StatusCode { get; set; }
+
         public string Message { get; set; }
-        public string Details { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
     }
 }
